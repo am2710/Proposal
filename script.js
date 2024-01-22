3c84f7ddb50e5484026da1a8d2333d38b274919c
am2710 <157260152+am2710@users.noreply.github.com>
2024-01-22 04:13:02 -0330

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,4 @@ function runAway() {
   alert("Oh no! The 'No' option ran away!");
 }
 
-function moveNo() {
-  const noButton = document.getElementById("noButton");
-  const randomX = Math.floor(Math.random() * 21) - 10; // Random number between -10 and 10
-  const randomY = Math.floor(Math.random() * 21) - 10; // Random number between -10 and 10
-  noButton.style.transform = `translate(${randomX}px, ${randomY}px)`;
-}
+
